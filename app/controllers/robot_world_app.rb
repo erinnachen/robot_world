c60617073960011c23600fbaf0256ea0d3b2ee6d
Erinna Chen <erinna.chen@gmail.com>
2016-02-03 22:07:35 -0700

    Building out create and read functionality in Robot World

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,3 +1,6 @@
+require 'models/robot_roster'
+require 'yaml/store'
+
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
@@ -5,8 +8,21 @@ class RobotWorldApp < Sinatra::Base
     erb :dashboard
   end
 
+  get '/robots' do
+    @robots = robot_roster.all
+    erb :index
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
   not_found do
    erb :error
   end
 
+  def robot_roster
+    database = YAML::Store.new("db/robot_roster")
+    RobotRoster.new(database)
+  end
 end
